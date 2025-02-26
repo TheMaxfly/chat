b67b004929542add53f8f7ad692a82eb61a18aad
flavigny maxime <maxime.mcfly@gmail.com>
2025-02-26 16:19:46 +0100

    correction acces conseillers

--- chat/urls.py
+++ chat/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'chat'
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Vue pour l'URL racine
+    #path('', views.index, name='index'),  # Vue pour l'URL racine
+    path('', views.advisor_list, name='advisor_list'),
     path('start_conversation/<int:advisor_id>/', views.start_conversation, name='start_conversation'),
     path('conversation/<int:conversation_id>/', views.conversation_detail, name='conversation_detail'),
 ]
